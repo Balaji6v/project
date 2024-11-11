56f9b0f210b25a903ab39f6bc8ad9fae1a9bcbb5
Balaji6v <119289607+Balaji6v@users.noreply.github.com>
2024-11-11 12:16:01 +0530

    Add files via upload

---
+++ unique character.py
@@ -0,0 +1,5 @@
+string = input("Enter a string: ")
+unique_characters = set(string)  # Convert string to a set to keep only unique characters
+unique_count = len(unique_characters)
+
+print("Number of unique characters:", unique_count)
